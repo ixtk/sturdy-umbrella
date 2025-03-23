380c420958bcaa64120bbe18afebbe604d9bddf1
ixtk <ikhcamot@gmail.com>
2025-03-23 13:30:18 +0400

    Add user register

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,45 @@
 import express from "express"
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema(
+  {
+    username: String,
+    email: String,
+    password: String
+  },
+  { timestamps: true }
+)
+
+export const User = mongoose.model("User", userSchema)
 
 const app = express()
-const port = 3000
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" })
+app.use(express.json())
+
+app.post("/users/register", async (req, res) => {
+  const { username, email, password, confirmPassword } = req.body
+
+  const saltRounds = 10
+  const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+  const newUser = new User({
+    username: username,
+    email,
+    password: hashedPassword
+  })
+
+  await newUser.save()
+
+  res.status(201).json(newUser)
 })
 
-app.listen(port, () => console.log(`Running on port ${port}`))
+app.listen(3000, async () => {
+  console.log("Running on port 3000")
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/sturdy-umbrella")
+    console.log("Connected to the database")
+  } catch (error) {
+    console.log(error)
+  }
+})
