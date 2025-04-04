31d8afa5b3ff3753ac5e9fd9d0c81eb0d8baf903
vakogabiani <vakogabiani@gmail.com>
2025-04-04 23:56:41 +0400

    basic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.scss"
 import { Route, Routes } from "react-router"
-import { HomePage } from "./pages/Home"
+import ProductList from "./pages/Home"
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index element={<ProductList />} />
     </Routes>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,31 @@
 import "./Home.scss"
+import { useEffect, useState } from "react"
 
-export const HomePage = () => {
-  return <h1 className="title">Home page</h1>
+export const ProductList = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.products)
+      })
+      .catch(error => {
+        console.error("Error fetching products:", error)
+      })
+  }, [])
+  return (
+    <div className="product-list">
+      {products.map(product => (
+        <div key={product.id} className="product-card">
+          <img src={product.imageUrl} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+export default ProductList
