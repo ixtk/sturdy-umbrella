2b6ec5beac7401ff2893f60e8f222fd8eb1529be
giorgi-jakhua <jaxuagiorgi5@gmail.com>
2025-04-27 11:22:57 +0400

    Saving changes before switching branches

--- frontend/src/pages/EditProduct.jsx
+++ frontend/src/pages/EditProduct.jsx
@@ -1,233 +1,162 @@
 import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { Formik, Form, Field, FieldArray } from "formik"
+// field ასრულებს ინპუტის ფუნქციას, რომელიც პირდაპირ formik-ს უკავშირდება. name ატრიბუტში ვწერთ key-ს სახელს, რომელიც გვინდა რომ შეცვალოს მოცემულმა ინპუტმა.
 import productData from "./fake.json"
 import "./EditProduct.scss"
+
 export default function EditProductPage() {
   const { productId } = useParams()
-  const [product, setProduct] = useState(null)
-
-  useEffect(() => {
-    if (productId) {
-      const foundProduct = productData.find(item => item.id === productId)
-      console.log("Found product:", foundProduct)
-      setProduct(foundProduct)
-    }
-  }, [productId])
-
-  if (!product) {
-    return <p>Loading...</p>
-  }
-
+  // useParams გვეხმარბა პროდუქტის id გამოვიყენოთ route-ში, მას გადააქცევს ცვლადად და საძიებო ველში მნიშვნელობას გამოიტანს.
   return (
     <div>
       <h1>Edit Product</h1>
-
-      <p className="product-name-header">Product Name</p>
-      <input
-        className="product-name"
-        type="text"
-        value={product.productName}
-        onChange={e => setProduct({ ...product, productName: e.target.value })}
-      />
-
-      <p className="category-header">Category</p>
-      <select
-        value={product.category}
-        onChange={e => setProduct({ ...product, category: e.target.value })}
+      <Formik
+        initialValues={productData.find(item => item.id === productId) || {
+          productName: "",
+          category: "",
+          pricing: { regularPrice: "", salePrice: "" },
+          onSale: false,
+          variants: [],
+        }}
+        // formik-ში გვიწერია default ობიექტი როგორი უნდა იყოს და გამოვიყენებთ default-ს თუ id-ს შესაბამისი პროდუქტი არ გვაქვს, თუ გვაქვს მოვძებნით და მასში შემავალ მნიშვნელობებს გამოვიყენებთ default-ად.
+        enableReinitialize
+        // ეს გვაძლევს უფლებას შევცვალოთ საწყისი მნიშვნელობები
       >
-        {product.categories.map((category, index) => (
-          <option key={index} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
+        {({ values, setFieldValue }) => (
+          <Form>
+            <p className="product-name-header">Product Name</p>
+            <Field name="productName" className="product-name" type="text" />
 
-      <h1 className="pricing-header">Pricing</h1>
-      <div className="pricing">
-        <div className="regular-price">
-          <p>Regular Price ($)</p>
-          <input
-            type="text"
-            value={product.pricing.regularPrice}
-            onChange={e =>
-              setProduct({
-                ...product,
-                pricing: {
-                  ...product.pricing,
-                  regularPrice: e.target.value
-                }
-              })
-            }
-          />
-        </div>
-        <div className="sale-price">
-          <p>Sale Price ($)</p>
-          <input
-            type="text"
-            value={product.pricing.salePrice}
-            onChange={e =>
-              setProduct({
-                ...product,
-                pricing: {
-                  ...product.pricing,
-                  salePrice: e.target.value
-                }
-              })
-            }
-          />
-        </div>
-        <div
-          className="on-sale"
-          onClick={() =>
-            setProduct(prev => ({ ...prev, onSale: !prev.onSale }))
-          }
-        >
-          <div
-            className={`toggle-switch ${product.onSale ? "active" : ""}`}
-          ></div>
-          <p>{product.onSale ? "On Sale" : "Not on Sale"}</p>
-        </div>
-      </div>
+            <p className="category-header">Category</p>
+            <Field as="select" className="category-select" name="category">
+              {values.categories?.map((category, index) => (
+                <option key={index} value={category}>{category}</option>
+              ))}
+            </Field>
 
-      <div className="variants-header">
-        <h1>Variants</h1>
-        <button>
-          <i className="fi fi-rr-plus-small"></i> Add Variant
-        </button>
-      </div>
-      {product.variants.map((variant, index) => (
-        <div className="variants" key={index}>
-          <div className="variant-details">
-            <p>Variant Details</p>
-            <i class="fi fi-rs-trash"></i>
-          </div>
-          <div className="colorName-quantity-images">
-            <div className="colorName">
-              <p>Color Name</p>
-              <input
-                type="text"
-                value={variant.colorName}
-                onChange={e => {
-                  const updatedVariants = [...product.variants]
-                  updatedVariants[index] = {
-                    ...variant,
-                    colorName: e.target.value
-                  }
-                  setProduct({ ...product, variants: updatedVariants })
-                }}
-              />
-            </div>
-            <div className="quantity">
-              <p>Stock Quantity</p>
-              <input
-                type="text"
-                value={variant.stockQuantity}
-                onChange={e => {
-                  const updatedVariants = [...product.variants]
-                  updatedVariants[index] = {
-                    ...variant,
-                    stockQuantity: e.target.value
-                  }
-                  setProduct({ ...product, variants: updatedVariants })
-                }}
-              />
-            </div>
-            <div className="uploadImages">
-              <p>Images</p>
-              <button>
-                <i className="fi fi-rr-upload"></i> Upload Images
-              </button>
-            </div>
-            <div className="image-previews">
-              <p>Image Previews</p>
-              <div className="images">
-                {variant.images.map((image, i) => (
-                  <img key={i} src={image} alt="" />
-                ))}
+            <h1 className="pricing-header">Pricing</h1>
+            <div className="pricing">
+              <div className="regular-price">
+                <p>Regular Price ($)</p>
+                <Field className="regular-price-input" name="pricing.regularPrice" type="text" />
+              </div>
+              <div className="sale-price">
+                <p>Sale Price ($)</p>
+                <Field className="sale-price-input" name="pricing.salePrice" type="text" />
+              </div>
+              <div
+                className="on-sale"
+                onClick={() => setFieldValue("onSale", !values.onSale)}
+              >
+                <div className={`toggle-switch ${values.onSale ? "active" : ""}`}></div>
+                <p>{values.onSale ? "On Sale" : "Not on Sale"}</p>
               </div>
             </div>
-          </div>
 
-          <div className="sizes">
-            <div className="available-sizes">
-              <p>Available Sizes</p>
-              <button>
-                <i className="fi fi-rr-plus-small"></i> Add Size
-              </button>
+            <FieldArray name="variants">
+            {/* FieldArray გვაძლევს საშუალებას ვიმუშაოთ სიებთან და გვაძლევს დამატება და წაშლის ფუნქციებს. name="variants" არის ის სია რომელზეც მუშაობა გვინდა. fake.json-ში ერთ-ერთი key არის variants რომლის მნიშვნელობაც არის სია, სწორედ ამ სიასთან გვინდა მუშაობა. */}
+              {({ push, remove }) => (
+                <>
+                  <div className="variants-header">
+                    <h1>Variants</h1>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        push({
+                          colorName: "",
+                          stockQuantity: "",
+                          images: [],
+                          availableSizes: []
+                        })
+                      }
+                    >
+                      {/* ამ ღილაკით ვამატებთ fake.json-ში ახალ ობიექტს, სადაც საწყისი მნიშვნელობები ცარიელია */}
+                      <i className="fi fi-rr-plus-small"></i> Add Variant
+                    </button>
+                  </div>  
+                  {values.variants.map((variant, index) => (
+                    <div className="variants" key={index}>
+                      <div className="variant-details">
+                        <p>Variant Details</p>
+                        <button className="remove-variant-btn" onClick={() => remove(index)}>
+                        {/* remove(index) არის FieldArray-ს ფუნქცია რომელიც შლის ვარიანტს შესაბამისი ინდექსის მიხედვით */}
+                          <i className="fi fi-rs-trash"></i>
+                        </button>
+                      </div>
+
+                      <div className="colorName-quantity-images">
+                        <div className="colorName">
+                          <p>Color Name</p>
+                          <Field className="color-name-input" name={`variants[${index}].colorName`} type="text" />
+                        </div>
+                        <div className="quantity">
+                          <p>Stock Quantity</p>
+                          <Field className="stock-quantity-input" name={`variants[${index}].stockQuantity`} type="text" />
+                          {/* [${index}] გვეხმარება შესაბამისი ინდექსის შესაბამისი item შევცვალოთ ამ შემთხვევაში stockQuantity  */}
+                        </div>
+                        <div className="uploadImages">
+                          <p>Images</p>
+                          <button type="button">
+                            <i className="fi fi-rr-upload"></i> Upload Images
+                          </button>
+                        </div>
+                        <div className="image-previews">
+                          <p>Image Previews</p>
+                        </div>
+                      </div>
+
+                      <div className="sizes">
+                        <div className="available-sizes">
+                          <p>Available Sizes</p>
+                          <button
+                            type="button"
+                            onClick={() => setFieldValue(`variants[${index}].availableSizes`, [...variant.availableSizes, { mensSize: "", womensSize: "" }])}
+                          >
+                          {/* ამ ღილაკით ვამატებთ ახალ ზომას. */}
+                            <i className="fi fi-rr-plus-small"></i> Add Size
+                          </button>
+                        </div>
+                        {variant.availableSizes.map((size, sizeIndex) => (
+                          <div className="men-women-size" key={sizeIndex}>
+                            <div className="men-size">
+                              <p>Men's Size</p>
+                              <Field className="mens-size-input" name={`variants[${index}].availableSizes[${sizeIndex}].mensSize`} type="text" />
+                            </div>
+                            <div className="women-size">
+                              <p>Women's Size</p>
+                              <Field className="womens-size-input" name={`variants[${index}].availableSizes[${sizeIndex}].womensSize`} type="text" />
+                            </div>
+                            <button
+                              type="button"
+                              onClick={() => setFieldValue(`variants[${index}].availableSizes`, variant.availableSizes.filter((size, i) => i !== sizeIndex))}
+                            >
+                              <i className="fi fi-rr-cross-small"></i>
+                            </button>
+                            {/* ეს ღილაკი შლის ზომას შემდეგნაირად: ფილტრით აკეთებს ახალ სიას, სადაც შესაბამის ინდექსზე მყოფი ზომა აღარ იქნება */}
+                          </div>
+                        ))}
+                      </div>
+
+                      <p className="size-summary-header">Size Summary</p>
+                      <div className="size-summary">
+                        {variant.availableSizes.map((size, i) => (
+                          <div key={i}>
+                            <p>M {size.mensSize}/W {size.womensSize}</p>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+                </>
+              )}
+            </FieldArray>
+
+            <div className="save-changes-box">
+              <button className="save-changes" type="submit">Save Changes</button>
             </div>
-            {variant.availableSizes.map((size, sizeIndex) => (
-              <div className="men-women-size" key={sizeIndex}>
-                <div className="men-size">
-                  <p>Men's Size</p>
-                  <input
-                    type="text"
-                    value={size.mensSize}
-                    onChange={e => {
-                      const updatedVariants = [...product.variants]
-                      const updatedSizes = [...variant.availableSizes]
-                      updatedSizes[sizeIndex] = {
-                        ...size,
-                        mensSize: e.target.value
-                      }
-                      updatedVariants[index] = {
-                        ...variant,
-                        availableSizes: updatedSizes
-                      }
-                      setProduct({ ...product, variants: updatedVariants })
-                    }}
-                  />
-                </div>
-                <div className="wemen-size">
-                  <p>Women's Size</p>
-                  <input
-                    type="text"
-                    value={size.womensSize}
-                    onChange={e => {
-                      const updatedVariants = [...product.variants]
-                      const updatedSizes = [...variant.availableSizes]
-                      updatedSizes[sizeIndex] = {
-                        ...size,
-                        womensSize: e.target.value
-                      }
-                      updatedVariants[index] = {
-                        ...variant,
-                        availableSizes: updatedSizes
-                      }
-                      setProduct({ ...product, variants: updatedVariants })
-                    }}
-                  />
-                </div>
-                <button
-                  onClick={() => {
-                    const updatedVariants = [...product.variants]
-                    const updatedSizes = variant.availableSizes.filter(
-                      (_, i) => i !== sizeIndex
-                    )
-                    updatedVariants[index] = {
-                      ...variant,
-                      availableSizes: updatedSizes
-                    }
-                    setProduct({ ...product, variants: updatedVariants })
-                  }}
-                >
-                  <i className="fi fi-rr-cross-small"></i>
-                </button>
-              </div>
-            ))}
-          </div>
-
-          <p className="size-summary-header">Size Summary</p>
-          <div className="size-summary">
-            {variant.availableSizes.map((size, i) => (
-              <div key={i}>
-                <p>
-                  M {size.mensSize}/W {size.womensSize}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
