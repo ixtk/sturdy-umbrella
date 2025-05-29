91a5795054b0ebb2ad8b6916b28eef12d212ddcb
ixtk <ikhcamot@gmail.com>
2025-05-29 18:21:52 +0400

    Remove unused packages

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,13 @@
 import express from "express"
-import session from "express-session"
-import MongoStore from "connect-mongo"
 import mongoose from "mongoose"
 import { productRouter } from "./routers/productRouter.js"
 import { userRouter } from "./routers/userRouter.js"
 import cors from "cors"
-import { config } from "dotenv"
-
-config()
 
 export const app = express()
 
 const MONGODB_URL = "mongodb://127.0.0.1:27017/fluffy-umbrella"
 
-app.use(
-  session({
-    // HIDE IN PROD!
-    secret: "super secret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 3,
-      httpOnly: true
-    },
-    store: MongoStore.create({
-      mongoUrl: MONGODB_URL,
-      stringify: false
-    })
-  })
-)
 app.use(express.json())
 app.use(
   cors({
